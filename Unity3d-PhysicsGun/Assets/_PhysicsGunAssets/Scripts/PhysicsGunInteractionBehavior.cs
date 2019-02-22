e5124c5b76aa1e1540a5d7d91df165cc664b462c
Laumania <mads@laumania.net>
2019-02-22 21:30:51 +0100

    Added Controls-Instructions UI

--- Unity3d-PhysicsGun/Assets/_PhysicsGunAssets/Scripts/PhysicsGunInteractionBehavior.cs
+++ Unity3d-PhysicsGun/Assets/_PhysicsGunAssets/Scripts/PhysicsGunInteractionBehavior.cs
@@ -226,7 +226,7 @@ public class PhysicsGunInteractionBehavior : MonoBehaviour
             var toDestination = centerDestination - _grabbedRigidbody.transform.position;
 
             // Calculate force
-            var force = (toDestination / Time.fixedDeltaTime * 0.8f) / _grabbedRigidbody.mass;
+            var force = (toDestination / Time.fixedDeltaTime * 0.1f) / _grabbedRigidbody.mass;
 
             //force += _scrollWheelInput;
             // Remove any existing velocity and add force to move to final position
